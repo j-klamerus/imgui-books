09d64dcefd21e5a21f24d3a51796e876a68c147e
j-klamerus <klamerusjc@gmail.com>
2026-06-17 18:07:21 -0400

    added table of buttons for history, added unique ids for each button

--- src/history.cpp
+++ src/history.cpp
@@ -3,6 +3,35 @@
 
 namespace NoteHistory {
     void DisplayHistory() {
+        const int NUMBER_OF_COLUMNS = 73;
+        int boxID = 0;
+        bool checkboxBool = false;
         ImGui::Text("This is the history UI");
+            if(ImGui::BeginTable("split", 73)) {
+                for(int i = 0; i < NUMBER_OF_COLUMNS; i++) {
+                    ImGui::TableNextColumn();
+                    ImGui::PushID(boxID);
+                        ImGui::Button("", {25, 25});
+                    ImGui::PopID();
+                    boxID++;
+                    ImGui::PushID(boxID);
+                        ImGui::Button("", {25, 25});
+                    ImGui::PopID();
+                    boxID++;
+                    ImGui::PushID(boxID);
+                        ImGui::Button("", {25, 25});
+                    ImGui::PopID();
+                    boxID++;
+                    ImGui::PushID(boxID);
+                        ImGui::Button("", {25, 25});
+                    ImGui::PopID();
+                    boxID++;
+                    ImGui::PushID(boxID);
+                        ImGui::Button("", {25, 25});
+                    ImGui::PopID();
+                    boxID++;
+                }
+            ImGui::EndTable();
+        }
     }
 }
