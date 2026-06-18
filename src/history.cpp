f86617e82ce906c9e20931d9e4fe1210a8287553
j-klamerus <klamerusjc@gmail.com>
2026-06-17 21:22:12 -0400

    fixed colors on history

--- src/history.cpp
+++ src/history.cpp
@@ -1,17 +1,25 @@
 #include "history.h"
 #include "imgui.h"
+#include <vector>
 
 namespace NoteHistory {
+    const int NUMBER_OF_COLUMNS = 73;
+    int boxID = 0;
+    bool checkboxBool = false;
+    //declare history button color shades
+    std::vector<std::vector<float>> shades = {
+        {},
+        {},
+        {},
+        {},
+        {}
+    };
     void DisplayHistory() {
-        const int NUMBER_OF_COLUMNS = 73;
-        int boxID = 0;
-        bool checkboxBool = false;
-
             if(ImGui::BeginTable("split", 73)) {
                 for(int i = 0; i < NUMBER_OF_COLUMNS; i++) {
 
-                    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(40.0f, 234.0f, 64.0f, 1.0f));
-                    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(40.0f, 234.0f, 64.0f, 0.5f));
+                    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(52.0f / 255.0f, 152.0f / 255.0f, 219.0f / 255.0f, 1.0f));
+                    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(52.0f / 255.0f, 152.0f / 255.0f, 219.0f / 255.0f, 1.0f));
                     
                     ImGui::TableNextColumn();
                     ImGui::PushID(boxID);
