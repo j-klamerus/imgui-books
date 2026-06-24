014b78f8e302b221ccc7e4262136e3a16a3efcc4
j-klamerus <klamerusjc@gmail.com>
2026-06-23 23:56:22 -0400

    addbook input setup

--- src/addBook.cpp
+++ src/addBook.cpp
@@ -1,19 +1,42 @@
 #include "addBook.h"
 #include "imgui.h"
 #include <iostream>
+#include "book_data.h"
 
 namespace AddBook {
+
+    BookData bookData;
+
+    int firstPage = 0;
+    int endPage = 0;
+
     void DisplayAddPage() {
+
         //display text input and define labeles for each
         static char bookTitleLabel[255] = "Book Title";
-        ImGui::InputText("##edit", bookTitleLabel, IM_COUNTOF(bookTitleLabel));
+        if(ImGui::InputText("##edit", bookTitleLabel, IM_COUNTOF(bookTitleLabel))) {
+            bookData.title = bookTitleLabel;
+            std::cout << bookData.title << std::endl;
+        }
 
         static char readDescriptionLabel[255] = "description";
-        ImGui::InputText("##description", readDescriptionLabel, IM_COUNTOF(readDescriptionLabel));
+        if (ImGui::InputText("##description", readDescriptionLabel, IM_COUNTOF(readDescriptionLabel))) {
+            bookData.note = readDescriptionLabel;
+        }
 
         static char firstPageLabel [14] = "Page Started";
-        //int firstPage = 10;
-        static int vec4i[4] = { 1, 5, 100, 255 };
-        ImGui::InputInt2(firstPageLabel, vec4i);
+
+        ImGui::InputInt(firstPageLabel, &firstPage);
+        bookData.pageStart = firstPage;
+
+        static char endPageLabel [14] = "Page Ended";
+    
+        ImGui::InputInt(endPageLabel, &endPage);
+        bookData.pageEnd = endPage;
+
+        if(ImGui::Button("print page start and end")) {
+            std::cout << bookData.pageStart << std::endl;
+            std::cout << bookData.pageEnd << std::endl;
+        }
     };
 }
