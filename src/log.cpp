f680a8ac87daf16105b750ddc160adfbfa4939e8
j-klamerus <klamerusjc@gmail.com>
2026-06-29 22:58:44 -0400

    dump reading log to screen

--- src/log.cpp
+++ src/log.cpp
@@ -11,6 +11,8 @@ namespace Log {
     BookData bookData;
     bool dataFetched;
 
+    std::vector<BookData> readingLog = {};
+
     void DisplayReadingLog(AppState& state) {
         ImGui::Text("Reading Log!!!!");
         if(!state.initializedReadingLog) {
@@ -19,21 +21,20 @@ namespace Log {
         }
         //dataFetched = true;
         if(dataFetched) {
-            /*
-            ImGui::Text("%s", bookData.title.c_str());
-            ImGui::Text("%s", bookData.note.c_str());
-            ImGui::Text("%s", bookData.date.c_str());
-            ImGui::Text("Started on page: %d, Finished on page: %d\n", bookData.pageStart, bookData.pageEnd);
-            */
+            for(const auto& read : readingLog) {
+                ImGui::Text("%s", read.title.c_str());
+                ImGui::Text("%s", read.note.c_str());
+                ImGui::Text("%s", read.date.c_str());
+                ImGui::Text("Read %d pages.", read.pageEnd - read.pageStart);
+            }
            ImGui::Text("data fetched is true");
-            std::cout << "is this going??" << std::endl;
         }
     }
 
     void fetchUserData() {
         //dataFetched = true;
         std::ifstream file("/Users/klamerus/HOME/imgui_book/save_data/user_data.json");
-
+        
         if (!file.is_open()) {
         printf("failed to open json file\n");
         return;
@@ -46,8 +47,19 @@ namespace Log {
             bookData.note = j["note"];
             bookData.pageStart = j["page_start"];
             bookData.pageEnd = j["page_end"];*/
-            bookData.title = j["reads"][0]["title"];
-            printf("%s\n", bookData.title.c_str());
+            for(int i = 0; i < j["reads"].size(); i++) {
+                readingLog.push_back({j["reads"][i]["title"],
+                    j["reads"][i]["note"],
+                    j["reads"][i]["page_start"],
+                    j["reads"][i]["page_end"],
+                    j["reads"][i]["date"],
+                    j["reads"][i]["id"]
+                });
+                //bookData.title = j["reads"][i]["title"];
+                //printf("%s\n", bookData.title.c_str());
+            }
+            dataFetched = true;
+            //printf("%s\n", bookData.title.c_str());
             //printf("%s\n", bookData.note.c_str());
             //printf("%s\n", bookData.date.c_str());
             //printf("Started on page: %d, Finished on page: %d\n", bookData.pageStart, bookData.pageEnd);
